2aa62bc8cee8f241eededc82696e3f74def054a4
John Duncan <john@johnduncanphoto.com>
2017-12-01 17:26:29 +0000

    Basket Test created, items implement buyable interface

---
+++ app/src/main/java/com/example/johnd/shoppingbasket/Item/Hubs.java
@@ -0,0 +1,10 @@
+package com.example.johnd.shoppingbasket.Item;
+
+import com.example.johnd.shoppingbasket.Buyable;
+
+/**
+ * Created by JohnD on 01/12/2017.
+ */
+
+public class Hubs implements Buyable {
+}
---
+++ app/src/test/java/com/example/johnd/shoppingbasket/BasketUnitTest.java
@@ -0,0 +1,8 @@
+package com.example.johnd.shoppingbasket;
+
+/**
+ * Created by JohnD on 01/12/2017.
+ */
+
+public class BasketUnitTest {
+}
