945cf39e5067fd169565e535524c1a2091d8cb26
John Duncan <john@johnduncanphoto.com>
2017-12-02 18:56:01 +0000

    Can add multiple Items to basket, using buyable interface.

--- app/src/test/java/com/example/johnd/shoppingbasket/BasketUnitTest.java
+++ app/src/test/java/com/example/johnd/shoppingbasket/BasketUnitTest.java
@@ -1,6 +1,7 @@
 package com.example.johnd.shoppingbasket;
 
 import com.example.johnd.shoppingbasket.Item.Brakes;
+import com.example.johnd.shoppingbasket.Item.Tyre;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -21,12 +22,22 @@ public class BasketUnitTest {
     }
 
     @Test
-    public void canAddItemTOBasket(){
+    public void canAddItemToBasket(){
         Basket basket = new Basket();
         Brakes brake = new Brakes();
         brake.setPrice(75);
         brake.setname("Avid juicy");
         basket.addItem(brake);
         assertEquals(1,basket.getBasket().size());
+    }
+
+    @Test
+    public void canAddItemsOfDifferentTypeToBasket(){
+        Basket basket = new Basket();
+        Brakes brake = new Brakes();
+        Tyre tyre = new Tyre();
+        basket.addItem(brake);
+        basket.addItem(tyre);
+        assertEquals(2,basket.getBasket().size());
     }
 }
