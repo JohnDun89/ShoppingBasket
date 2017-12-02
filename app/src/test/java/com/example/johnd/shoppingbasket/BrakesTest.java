eaccd9376f421d83c8193d40818a9f801dc38389
John Duncan <john@johnduncanphoto.com>
2017-12-02 17:25:47 +0000

    Cna get and set Brake name, test passed

--- app/src/test/java/com/example/johnd/shoppingbasket/BrakesTest.java
+++ app/src/test/java/com/example/johnd/shoppingbasket/BrakesTest.java
@@ -1,8 +1,28 @@
 package com.example.johnd.shoppingbasket;
 
+import com.example.johnd.shoppingbasket.Item.Brakes;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by JohnD on 01/12/2017.
  */
 
 public class BrakesTest {
+
+    Brakes brake;
+
+    @Before
+    public void setUo(){
+        brake = new Brakes();
+    }
+
+    @Test
+    public void canSetBrakeName(){
+        brake.setname("Shimano XT front");
+        assertEquals(brake.getName(),"Shimano XT front");
+    }
 }
